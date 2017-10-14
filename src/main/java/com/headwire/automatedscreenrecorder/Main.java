72954271b6c653d1c69913f6336f7eb3678c17c1
PascalBurri <pascal.burri.01@stud.hslu.ch>
2017-10-14 23:56:17 +0200

    Deleted External library monte

--- src/main/java/com/headwire/automatedscreenrecorder/Main.java
+++ src/main/java/com/headwire/automatedscreenrecorder/Main.java
@@ -16,7 +16,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		if(args.length < 2) {
-			System.out.println("automatedscreenrecorder <script> <driverpath> <timeout>");
+			System.out.println("automatedscreenrecorder <script> <driverpath>");
 		} else {
 			setDriverPath(args[1]);
 			readFile(args[0]);
